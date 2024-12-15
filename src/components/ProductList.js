a28e0dcc5ed0d0234fffad5d4d30084739f11bfb
Rifki Syahada <rifkisyahada81@gmail.com>
2024-12-16 01:11:07 +0700

    category product

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,7 +3,7 @@ import Product from './Product';
 import Cart from './Cart';
 
 const ProductList = () => {
-  const [products, setProducts] = useState([]);
+  const [product, setProducts] = useState([]);
   const apiUrl = process.env.REACT_APP_API_URL;
 
   useEffect(() => {
@@ -12,15 +12,30 @@ const ProductList = () => {
       .then(data => setProducts(data));
   }, []);
 
+  // produk bedasarkan kategori
+  const groupedProducts = product.reduce((acc, product) => {
+    const category = product.category || 'Lainnya';
+    if(!acc[category]){
+      acc[category] = [];
+    }
+    acc[category].push(product);
+    return acc;
+  }, {});
+
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold">Daftar Produk</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {products.map(product => (
-          <Product key={product.id} product={product} />
-        ))}
-      </div>
-      <Cart/>
+      {Object.keys(groupedProducts).map(category => (
+        <div key={category} className="mb-8">
+          <h2 className="text-2xl font-semibold">{category}</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {groupedProducts[category].map(product => (
+              <Product key={product.id} product={product} />
+            ))}
+          </div>
+        </div>
+      ))}
+      <Cart />
     </div>
   );
 };
