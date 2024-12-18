a7ee692524ef3db591caddf665776e8cc5933aa7
Rifki Syahada <rifkisyahada81@gmail.com>
2024-12-18 16:27:20 +0700

    style login

--- src/components/Login.js
+++ src/components/Login.js
@@ -63,7 +63,7 @@ const Login = () => {
     };
 
     return (
-        <section className="bg-gray-50">
+        <section className="bg-gray-50 mt-16">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                 <a
                     href="#"
