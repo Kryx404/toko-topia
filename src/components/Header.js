6d1eec3d92b2314867131d83ec69e6179d10a809
Rifki Syahada <rifkisyahada81@gmail.com>
2024-12-18 16:19:06 +0700

    tambah icon + logo header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../store/userSlice"; // Import action logout
 import Swal from "sweetalert2";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart, faRightToBracket, faRightFromBracket, faHouse } from "@fortawesome/free-solid-svg-icons";
 
 const Header = ({ onSearch }) => {
     const dispatch = useDispatch();
@@ -40,15 +42,16 @@ const Header = ({ onSearch }) => {
             className="fixed top-0 left-0 w-full text-black py-4 bg-white"
             style={{ zIndex: 1000 }}>
             <div className="container mx-auto flex items-center">
-                <div className="flex lg:flex-1">
-                    <a href="#" className="-m-1.5 p-1.5">
-                        <span className="sr-only">Your Company</span>
+                <div className="flex items-center lg:flex-1">
+                    <Link to="/" className="-m-1.5 p-1.5 flex items-center">
+                        {/* <span className="sr-only">Your Company</span> */}
                         <img
-                            alt=""
-                            src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-                            className="h-8 w-auto"
+                            alt="TokoTopia"
+                            src={`${process.env.PUBLIC_URL}/logo-tokotopia.png`}
+                            className="h-8 w-auto mr-3"
                         />
-                    </a>
+                        <h1 className="text-lg font-bold">TokoTopia</h1>{" "}
+                    </Link>
                 </div>
 
                 <div className="flex-grow mx-4">
@@ -59,43 +62,47 @@ const Header = ({ onSearch }) => {
                         onChange={handleSearchChange}
                         className="border rounded p-2 w-full md:w-96"
                     />
-                    </div>
-                    <nav className="flex justify-end">
-                        <ul className="flex justify-end gap-2 items-center">
+                </div>
+                <nav className="flex justify-end">
+                    <ul className="flex justify-end gap-2 items-center">
+                        <li className="mr-4">
+                            <Link
+                                to="/"
+                                className="text-black hover:text-blue-700">
+                                    <FontAwesomeIcon icon={faHouse} className="mr-2" />
+                                Homepage
+                            </Link>
+                        </li>
+                        {isLoggedIn && (
                             <li className="mr-4">
                                 <Link
-                                    to="/"
+                                    to="/cart"
                                     className="text-black hover:text-blue-700">
-                                    Homepage
+                                         <FontAwesomeIcon icon={faShoppingCart} className="mr-2" />
+                                    Keranjang
                                 </Link>
                             </li>
-                            {isLoggedIn && (
-                                <li className="mr-4">
-                                    <Link
-                                        to="/cart"
-                                        className="text-black hover:text-blue-700">
-                                        Keranjang
-                                    </Link>
-                                </li>
+                        )}
+                        <li>
+                            {isLoggedIn ? (
+                                <button
+                                    onClick={handleLogout}
+                                    className="text-white hover:bg-blue-500 py-2 px-6 bg-blue-700 rounded-lg">
+                                    Logout
+                                    <FontAwesomeIcon icon={faRightFromBracket} className="ml-2" />
+                                </button>
+                            ) : (
+                                <Link
+                                    to="/login"
+                                    className="text-white hover:bg-blue-500 py-2 px-6 bg-blue-700 rounded-lg">
+                                    Login
+                                        <FontAwesomeIcon icon={faRightToBracket} className="ml-2" />
+                                </Link>
                             )}
-                            <li>
-                                {isLoggedIn ? (
-                                    <button
-                                        onClick={handleLogout}
-                                        className="text-white hover:bg-blue-500 py-2 px-6 bg-blue-700 rounded-lg">
-                                        Logout
-                                    </button>
-                                ) : (
-                                    <Link
-                                        to="/login"
-                                        className="text-white hover:bg-blue-500 py-2 px-6 bg-blue-700 rounded-lg">
-                                        Login
-                                    </Link>
-                                )}
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     );
 };
