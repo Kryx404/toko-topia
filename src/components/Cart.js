d64d43efcc08d4fb5d6847742d1e212b8bbd7bff
Rifki Syahada <rifkisyahada81@gmail.com>
2024-12-19 04:02:36 +0700

    styling cart page

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import Swal from 'sweetalert2';
-import { removeFromCart, updateQuantity, clearCart } from '../store/cartSlice'; // Pastikan Anda memiliki action ini
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import { removeFromCart, updateQuantity, clearCart } from "../store/cartSlice"; // Pastikan Anda memiliki action ini
 
 const Cart = () => {
     const cartItems = useSelector((state) => state.cart);
@@ -10,27 +10,8 @@ const Cart = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
-
     const handleQuantityChange = (item, newQuantity) => {
-        // Hitung selisih antara jumlah baru dan jumlah lama
-        const quantityDifference = newQuantity - item.quantity;
-
-        // Update jumlah di cart
         dispatch(updateQuantity({ id: item.id, quantity: newQuantity }));
-
-        // Update stok di localStorage
-        const storedStock = localStorage.getItem(`stock_${item.id}`);
-        if (storedStock) {
-            const availableStock = JSON.parse(storedStock);
-            const newAvailableStock = availableStock - quantityDifference;
-
-            // Pastikan stok tidak menjadi negatif
-            if (newAvailableStock >= 0) {
-                localStorage.setItem(`stock_${item.id}`, JSON.stringify(newAvailableStock));
-            } else {
-                alert("Stok tidak cukup!");
-            }
-        }
     };
 
     const handleRemoveFromCart = (item) => {
@@ -40,77 +21,103 @@ const Cart = () => {
         if (storedStock) {
             const availableStock = JSON.parse(storedStock);
             const newAvailableStock = availableStock + item.quantity; // Kembalikan jumlah yang dihapus ke stok
-            localStorage.setItem(`stock_${item.id}`, JSON.stringify(newAvailableStock));
+            localStorage.setItem(
+                `stock_${item.id}`,
+                JSON.stringify(newAvailableStock),
+            );
         }
     };
 
     const handleCheckout = () => {
-let stockSufficient = true;
+        let stockSufficient = true;
 
-cartItems.forEach(item => {
-    const storedStock = localStorage.getItem(`stock_${item.id}`);
-    const availableStock = storedStock ? JSON.parse(storedStock) : 0;
+        cartItems.forEach((item) => {
+            const storedStock = localStorage.getItem(`stock_${item.id}`);
+            const availableStock = storedStock ? JSON.parse(storedStock) : 0;
 
-    if (item.quantity > availableStock){
-        stockSufficient = false;
-    }
-});
+            if (item.quantity > availableStock) {
+                stockSufficient = false;
+            }
+        });
 
-if (!stockSufficient){
-    Swal.fire ({
-        title: 'Stok Tidak Cukup',
-        text: 'Beberapa item di keranjang anda memiliki quantity yang melebihi stok',
-        icon: 'error',
-        confirmButtonText: 'ok',
-    });
-    return;
-}
-    // kurangi stok yang di local storage
-    cartItems.forEach(item => {
-        const storedStock = localStorage.getItem(`stock_${item.id}`);
-        const availableStock = storedStock ? JSON.parse(storedStock) : 0;
-        const newAvailableStock = availableStock - item.quantity;
+        if (!stockSufficient) {
+            Swal.fire({
+                title: "Insufficient Stock",
+                text: "Some items in your cart have quantities that exceed the stock",
+                icon: "error",
+                confirmButtonText: "OK",
+            });
+            return;
+        }
+        // kurangi stok yang di local storage
+        cartItems.forEach((item) => {
+            const storedStock = localStorage.getItem(`stock_${item.id}`);
+            const availableStock = storedStock ? JSON.parse(storedStock) : 0;
+            const newAvailableStock = availableStock - item.quantity;
 
-        localStorage.setItem(`stock_${item.id}`, JSON.stringify(newAvailableStock));
-    });
+            localStorage.setItem(
+                `stock_${item.id}`,
+                JSON.stringify(newAvailableStock),
+            );
+        });
 
-    // hapus cart after checkout
-    dispatch(clearCart());
+        // hapus cart after checkout
+        dispatch(clearCart());
 
-    Swal.fire({
-        title: 'Checkout Berhasil!',
-        text: 'Terima kasih telah berbelanja!',
-        icon: 'success',
-        confirmButtonText: 'OK'
-    }).then(() => {
-        navigate('/'); // Redirect to home page
-    });
-};
+        Swal.fire({
+            title: "Checkout Successful!",
+            text: "Thank you for shopping!",
+            icon: "success",
+            confirmButtonText: "OK",
+        }).then(() => {
+            navigate("/"); // Redirect to home page
+        });
+    };
 
     return (
-        <div className="container mx-auto p-4">
-            <h1 className="text-3xl font-bold">Keranjang Belanja</h1>
+        <div className="container mx-auto p-4 mt-16">
+            <h1 className="text-3xl font-bold mb-6">Shopping Cart</h1>
             {cartItems.length === 0 ? (
-                <p>Anda belum memilih item.</p>
+                <p className="mt-16 text-center font-semibold">You haven't selected any items.</p>
             ) : (
-               <div>
-               <ul>
-                    {cartItems.map(item => (
-                        <li key={item.id} className="border-b py-2 flex items-center">
-                            {item.title} - ${item.price} x 
-                            <input
-                                type="number"
-                                value={item.quantity}
-                                onChange={(e) => handleQuantityChange(item, Number (e.target.value))}
-                                className="border rounded w-20 mx-2"
-                                min="1"
-                            />
-                            <button onClick={() => handleRemoveFromCart(item)} className="text-red-500">Hapus</button>
-                        </li>
-                    ))}
-                </ul>
-                <button onClick={handleCheckout} className='mt-4 bg-green-500 text-white py-2 px-4 rounded'>Checkout</button>
-                
+                <div className="bg-white shadow-md rounded-lg p-6">
+                    <ul className="divide-y divide-gray-200">
+                        {cartItems.map((item) => (
+                            <li key={item.id} className="flex justify-between items-center py-4">
+                                <div className="flex items-center">
+                                    <img src={item.image} alt={item.title} className="w-16 h-16 object-contain rounded-md mr-4" />
+                                    <div>
+                                        <h2 className="text-lg font-semibold">{item.title}</h2>
+                                        <p className="text-gray-600">Price: ${item.price}</p>
+                                    </div>
+                                </div>
+                                <div className="flex items-center">
+                                    <input
+                                        type="number"
+                                        value={item.quantity}
+                                        onChange={(e) => handleQuantityChange(item, Number(e.target.value))}
+                                        className="border rounded w-20 py-1 px-2 text-center"
+                                        min="1"
+                                    />
+                                    <button
+                                        onClick={() => handleRemoveFromCart(item)}
+                                        className="ml-4 text-red-500 hover:text-red-700">
+                                        Remove
+                                    </button>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                    <div className="mt-4 flex justify-between items-center">
+                        <p className="text-lg font-semibold">
+                            Total: ${cartItems.reduce((total, item) => total + item.price * item.quantity, 0).toFixed(2)}
+                        </p>
+                        <button
+                            onClick={handleCheckout}
+                            className="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600 transition duration-200">
+                            Checkout
+                        </button>
+                    </div>
                 </div>
             )}
         </div>
